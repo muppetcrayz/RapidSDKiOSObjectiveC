2e36007b0182bc7cb4425452b4403180f9c4ba3c
Sage Conger <sconger@capital.edu>
2018-05-15 10:49:23 -0400

    Initial Commit

---
+++ MobileAPIObjectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MobileAPIObjectiveC
+//
+//  Created by Sage Conger on 5/15/18.
+//  Copyright © 2018 DUBTEL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MobileAPIObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MobileAPIObjectiveC
+//
+//  Created by Sage Conger on 5/15/18.
+//  Copyright © 2018 DUBTEL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
